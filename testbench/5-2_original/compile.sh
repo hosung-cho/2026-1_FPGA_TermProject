17e5a432b721c8f5b0cb878733267d4666ac90db
hosung.cho <hosung410@gmail.com>
2026-05-25 01:49:26 +0900

    compile.sh 수정

--- testbench/5-2_original/compile.sh
+++ testbench/5-2_original/compile.sh
@@ -7,7 +7,7 @@ cd "$SCRIPT_DIR"
 echo "========================================="
 # 1. Compile to ELF
 echo "[Build] Compiling lenet5_sw.c and startup.S..."
-/opt/riscv32i/bin/riscv32-unknown-elf-gcc -march=rv32i -mabi=ilp32 -O2 -nostdlib -fno-builtin -fno-tree-loop-distribute-patterns -T link.ld startup.S lenet5_sw.c -o lenet5.elf
+/opt/riscv32i/bin/riscv32-unknown-elf-gcc -march=rv32i -mabi=ilp32 -O2 -nostdlib -fno-builtin -fno-tree-loop-distribute-patterns -Wl,--no-check-sections -T link.ld startup.S lenet5_sw.c -o lenet5.elf
 
 # 2. Extract hex files
 echo "[Build] Extracting ROM and RAM hex files..."
